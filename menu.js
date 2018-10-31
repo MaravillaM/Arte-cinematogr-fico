efa361f4921fea36cdd8be7e2f586306e05646b1
Orellana <orellanaenrique841@gmail.com>
2018-10-31 03:35:00 -0600

    primer sitio web

---
+++ menu.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	var altura = $('.menu').offset().top;
+	
+	$(window).on('scroll', function(){
+		if ( $(window).scrollTop() > altura ){
+			$('.menu').addClass('menu-fixed');
+		} else {
+			$('.menu').removeClass('menu-fixed');
+		}
+	});
+
+});
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+$(document).ready(function(){
+	var altura = $('.menu').offset().top;
+	
+	$(window).on('scroll', function(){
+		if ( $(window).scrollTop() > altura ){
+			$('.menu').addClass('menu-fixed');
+		} else {
+			$('.menu').removeClass('menu-fixed');
+		}
+	});
+ 
+});
